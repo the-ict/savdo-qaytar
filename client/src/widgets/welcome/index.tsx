75096f9afe951dceec91d7b218850bfbf78de24c
avitzen <dvltinv@gmail.com>
2026-02-22 17:49:53 +0500

    refactor: reformat component import statements in Welcome widget.

--- client/src/widgets/welcome/index.tsx
+++ client/src/widgets/welcome/index.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import React from 'react';
-import { Hero } from './Hero';
-import { WhyUs } from './WhyUs';
-import { Statistics } from './Statistics';
-import { Pricing } from './Pricing';
-import { Reviews } from './Reviews';
+import {
+  Hero
+} from './Hero';
+import {
+  WhyUs
+} from './WhyUs';
+import {
+  Statistics
+} from './Statistics';
+import {
+  Pricing
+} from './Pricing';
+import {
+  Reviews
+} from './Reviews';
 
 const Welcome = () => {
   return (
