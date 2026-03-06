0a46a9cc429ce9de2a5c2e2f6077fe350e677838
avitzen <dvltinv@gmail.com>
2026-03-06 21:14:29 +0500

    refactor: Move klinecharts initialization and styling logic from BacktestingChart to BacktestingApp.

--- client/src/features/backtesting/ui/BacktestingApp.tsx
+++ client/src/features/backtesting/ui/BacktestingApp.tsx
@@ -16,6 +16,14 @@ import {
   Maximize2
 } from 'lucide-react';
 
+import {
+  useEffect,
+} from 'react';
+import {
+  init,
+  dispose,
+  CandleType,
+} from "klinecharts";
 
 const DRAWING_TOOLS = [
   { id: 'cursor', icon: <MousePointer2 className="w-4 h-4" />, name: 'Cursor' },
@@ -31,6 +39,51 @@ export default function BacktestingApp() {
   const handleChangeTimeFrame = (chart: any, newData: { timestamp: number, open: number, low: number, high: number, close: number, volume: number }[]) => {
     chart.applyNewData([])
   };
+
+  useEffect(() => {
+    const chart = init('chart');
+
+    chart?.applyNewData([
+      { timestamp: 1517846400000, open: 7424.6, high: 7511.3, low: 6032.3, close: 7310.1, volume: 224461 },
+      { timestamp: 1517932800000, open: 7310.1, high: 8499.9, low: 6810, close: 8165.4, volume: 148807 },
+      { timestamp: 1518019200000, open: 8166.7, high: 8700.8, low: 7400, close: 8245.1, volume: 24467 },
+      { timestamp: 1518105600000, open: 8244, high: 8494, low: 7760, close: 8364, volume: 29834 },
+      { timestamp: 1518192000000, open: 8363.6, high: 9036.7, low: 8269.8, close: 8311.9, volume: 28203 }
+    ]);
+
+    chart?.setStyles({
+      candle: {
+        type: CandleType.CandleSolid,
+        bar: {
+          upColor: '#2DC08E',
+          downColor: '#F92855',
+          noChangeColor: '#888888',
+          upBorderColor: '#2DC08E',
+          downBorderColor: '#F92855',
+          noChangeBorderColor: '#888888',
+          upWickColor: '#2DC08E',
+          downWickColor: '#F92855',
+          noChangeWickColor: '#888888'
+        },
+      },
+      xAxis: {
+        show: false,
+      },
+      yAxis: {
+        show: false,
+      },
+      grid: {
+        show: false,
+      },
+      crosshair: {
+        show: false,
+      }
+    });
+
+    return () => {
+      dispose('chart');
+    };
+  }, []);
 
   return (
     <div className="flex flex-col h-screen bg-[#131722] overflow-hidden">
@@ -52,7 +105,7 @@ export default function BacktestingApp() {
           </div>
         </div>
 
-        <BacktestingChart />
+        <div id='chart' className='w-full h-full'></div>
       </div>
     </div >
   );
